0f7cef295c6f221615480a483629cf2a668a3ca4
angelsmithlgs <angelsmithlgs@gmail.com>
2023-11-14 21:13:50 -0300

    filtro add

--- src/components/adminpac/AdminPac.jsx
+++ src/components/adminpac/AdminPac.jsx
@@ -1,18 +1,107 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './adminpac.css';
 import { Link } from 'react-router-dom';
+import '../adminpac/Data';
 
 import Filtro from './filter.svg';
 
 function AdminPac() {
+  const [filtroVisible, setFiltroVisible] = useState(false);
+
+  const [rutRunFilter, setRutRunFilter] = useState('');
+  const [nombreFilter, setNombreFilter] = useState('');
+  const [bancoFilter, setBancoFilter] = useState('');
+  const [numeroCuentaFilter, setNumeroCuentaFilter] = useState('');
+  const [montoFilter, setMontoFilter] = useState('');
+  const [productoFilter, setProductoFilter] = useState('');
+  const [codigoServicioFilter, setCodigoServicioFilter] = useState('');
+
+  const abrirFiltro = () => {
+    setFiltroVisible(true);
+  };
+
+  const cerrarFiltro = () => {
+    setFiltroVisible(false);
+  };
+
+  const filtrarDatos = () => {
+    console.log({
+      rutRunFilter,
+      nombreFilter,
+      bancoFilter,
+      numeroCuentaFilter,
+      montoFilter,
+      productoFilter,
+      codigoServicioFilter,
+    });
+  };
+
   return (
     <>
       <div className='admin-pac-container'>
         <div className='blue-rectangle'>Administrador PAC</div>
       </div>
-      <Link as={Link} to='/filtro'>
-        <img src={Filtro} alt='Logo' className='search-container' />
-      </Link>
+      <div>
+        <img
+          src={Filtro}
+          alt='Logo'
+          className='search-container'
+          onClick={abrirFiltro}
+        />
+      </div>
+      {/* AÑADIR SELECCIONADOR AL FILTRO, NO OLVIDAR */}
+      {filtroVisible && (
+        <div className='filtro-popup'>
+          <div>
+            <input
+              type='text'
+              placeholder='RUT/RUN'
+              value={rutRunFilter}
+              onChange={(e) => setRutRunFilter(e.target.value)}
+            />
+            <input
+              type='text'
+              placeholder='Nombre'
+              value={nombreFilter}
+              onChange={(e) => setNombreFilter(e.target.value)}
+            />
+            <input
+              type='text'
+              placeholder='Banco'
+              value={bancoFilter}
+              onChange={(e) => setBancoFilter(e.target.value)}
+            />
+            <input
+              type='text'
+              placeholder='N° Cuenta'
+              value={numeroCuentaFilter}
+              onChange={(e) => setNumeroCuentaFilter(e.target.value)}
+            />
+            <input
+              type='text'
+              placeholder='Monto ($)'
+              value={montoFilter}
+              onChange={(e) => setMontoFilter(e.target.value)}
+            />
+            <input
+              type='text'
+              placeholder='Producto'
+              value={productoFilter}
+              onChange={(e) => setProductoFilter(e.target.value)}
+            />
+            <input
+              type='text'
+              placeholder='Código Servicio'
+              value={codigoServicioFilter}
+              onChange={(e) => setCodigoServicioFilter(e.target.value)}
+            />
+          </div>
+
+          <button onClick={filtrarDatos}>Filtrar</button>
+          <button onClick={cerrarFiltro}>Cerrar</button>
+        </div>
+      )}
+
       <div>
         <div className='search-container'></div>
         <div className='form-container'>
